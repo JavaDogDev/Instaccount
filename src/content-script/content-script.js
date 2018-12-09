581d9384976230f11ac3cdd6774caf4dc5bae196
JavaDogDev <JavaDog.Dev@gmail.com>
2018-12-09 04:04:38 -0500

    Add guard var to content-script

--- src/content-script/content-script.js
+++ src/content-script/content-script.js
@@ -1,3 +1,15 @@
 import browser from 'webextension-polyfill';
 
-console.log(browser);
+(function() {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  console.log(browser);
+})();
