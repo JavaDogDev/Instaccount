e7d7bc207dc924720db5ea234fd5f880e2c3cce5
JavaDogDev <JavaDog.Dev@gmail.com>
2018-12-09 03:58:54 -0500

    Style popup and hook up inputs to JS

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,3 +1,20 @@
-function whatever() {
-  console.log('hi');
+function listenForRegisterClick() {
+  const inputElements = [
+    'username', 'password',
+    'first-name', 'last-name',
+    'email'
+  ];
+
+  const inputs = inputElements.map(i => document.getElementById(i))
+    .map(label => label.getElementsByTagName('input')[0]);
+
+  console.log(inputs);
 }
+
+function showError(err) {
+  console.error('uhh there was a problem sorry\n' + err);
+}
+
+browser.tabs.executeScript({ file: "content-script.bundle.js" })
+  .then(listenForRegisterClick)
+  .catch(showError);
